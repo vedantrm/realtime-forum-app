4da51a1de5367359c163041fb80647669e4789ac
vedantrm <45167999+vedantrm@users.noreply.github.com>
2019-03-24 18:54:01 +0000

    JWT package

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -56,7 +56,7 @@ class CategoryController extends Controller
     public function update(Request $request, Category $category)
     {
         $category->update([
-            'name' => $request->name, 
+            'name' => $request->name,
             'slug' => str_slug($request->name)
         ]);
         return response('Updated', Response::HTTP_ACCEPTED);
